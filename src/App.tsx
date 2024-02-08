fa913223eeb2a73e2cdb0472f5102286ed461556
stichmc <mcstich@outlook.com>
2024-02-08 11:01:44 -0700

    GUI init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,65 @@
+import { useState } from "react";
+import pickRandomName from "./controllers/random";
+import { Button } from "./components/ui/button";
+
+
+const namesArray: string[] = [
+  "Bob",
+  "Jeremy",
+  "Alice",
+  "Bob",
+  "Charlie",
+  "David",
+  "Eve",
+];
+
 const App = () => {
-  return <div>App</div>;
+  const [currentPickedName, setCurrentPickedName] = useState<string | null>(
+    null
+  );
+
+  const pickRandomNonConsecutiveName = () => {
+    let newPickedName = pickRandomName(namesArray);
+    while (newPickedName === currentPickedName) {
+      newPickedName = pickRandomName(namesArray);
+    }
+    setCurrentPickedName(newPickedName);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      {/* Display all names in big font */}
+      <div className="mb-8 text-4xl">
+        {namesArray.map((name, index) => (
+          <span key={index} className="mr-6">
+            {name}
+          </span>
+        ))}
+      </div>
+
+      {/* Display coffee image */}
+      <img
+        src={`${import.meta.env.BASE_URL}coffee.svg`}
+        alt="Cup"
+        className={`mb-4 h-40 transition-opacity ${
+          currentPickedName ? "opacity-100" : "opacity-0"
+        }`}
+      />
+
+      {/* Display current picked name */}
+      <div className="mb-4">
+        Current picked name: {currentPickedName ? currentPickedName : "None"}
+      </div>
+
+      {/* Pick Random Name Button */}
+      <Button
+        className="hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={pickRandomNonConsecutiveName}
+      >
+        Who will pay for coffee today?
+      </Button>
+    </div>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import App from "./App.tsx";
+
 // Components
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
